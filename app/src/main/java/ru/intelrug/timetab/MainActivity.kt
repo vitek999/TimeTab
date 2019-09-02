3a0e220e92234227466a03a93312c71ab2f2f6b9
vitek999 <vitek2012rus@gmail.com>
2019-09-02 21:31:04 +0400

    Set navigation to main activity

--- app/src/main/java/ru/intelrug/timetab/MainActivity.kt
+++ app/src/main/java/ru/intelrug/timetab/MainActivity.kt
@@ -2,11 +2,21 @@ package ru.intelrug.timetab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * Navigation
+         */
+        val navController = findNavController(R.id.nav_host_fragment)
+        findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            .setupWithNavController(navController)
     }
 }
